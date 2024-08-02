42bd4b230d1bac65d2f1902cbd839f8efed70d8c
Marcel Kessler <cheesenoc@gmail.com>
2024-08-02 23:32:14 +0200

    colors and font changed, icon not working in SB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule, MatButtonModule],
+  imports: [RouterOutlet, MatSlideToggleModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
